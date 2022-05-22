000f6492ba93bf2b58d25218496c5ca2960011b0
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-05-22 14:28:33 +0600

    Add inital SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,31 @@
 import NextNProgress from 'nextjs-progressbar';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
+import { DefaultSeo, NextSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <DefaultSeo
+        title="Anuar Shaidenov"
+        titleTemplate="Anuar Shaidenov"
+        defaultTitle="Anuar Shaidenov"
+        description="A front-end developer, passionate about UI and UX. I could help you boost your business and yurn your ideas to reality."
+        openGraph={{
+          title: 'Anuar Shaidenov',
+          description:
+            'A front-end developer, passionate about UI and UX. I could help you boost your business and yurn your ideas to reality.',
+          image: 'https://avatars.githubusercontent.com/u/45717589?v=4',
+          width: 800,
+          height: 420,
+          alt: 'Anuar Shaidenov',
+        }}
+        twitter={{
+          handle: '@anuarnyi',
+          cardType: 'summary_large_image',
+          site: '@anuarnyi',
+        }}
+      />
       <NextNProgress color="#F73E59" startPosition={0.9} height={2} />
       <Layout>
         <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from 'next/head';
 import HeroSection from '../components/sections/HeroSection';
 import ProjectsSections from '../components/sections/ProjectsSection';
+import { NextSeo } from 'next-seo';
 
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <>
+      <NextSeo title="Anuar's Portfolio | Home" />
       <HeroSection />
       <ProjectsSections />
     </>
