918409cca9050770890e0f42f5ac8d56955fe9eb
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-05-24 18:20:44 +0600

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,26 @@ import NextNProgress from 'nextjs-progressbar';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
 import { DefaultSeo, NextSeo } from 'next-seo';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="google" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+        `}
+      </Script>
       <DefaultSeo
         title="anuar shaidenov"
         defaultTitle="anuar shaidenov"
