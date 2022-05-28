062d4c199701400269ddcf60a721f731162cc251
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-05-28 18:51:51 +0600

    Add html lang attribute

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
       <link
